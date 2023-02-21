5637e67eac9c28b13025c19758b88670983841fa
Alex <lxkrmr@icloud.com>
2023-02-21 17:55:06 +0100

    💄 add some styling to UsersList

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -27,7 +27,13 @@ function UsersList() {
   }
 
   const renderedUsers = data.map((user) => {
-    return <div key={user.id}>{user.name}</div>;
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
   });
 
   return <div>{renderedUsers}</div>;
