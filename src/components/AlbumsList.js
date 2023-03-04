b53f9f077307c6637e772319b085f6267a87ec4e
Alex <lxkrmr@icloud.com>
2023-03-04 09:55:52 +0100

    ✨ introduce albums list

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,5 +1,36 @@
+import { useFetchAlbumsQuery } from '../store';
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
+
 function AlbumList({ user }) {
-  return <div>Albums for {user.name}</div>;
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  const content = (isLoading, error, data) => {
+    if (isLoading) {
+      return <Skeleton times={3} className="h-10 w-full" />;
+    }
+
+    if (error) {
+      return <div>Error loading albums.</div>;
+    }
+
+    return data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album.
+        </ExpandablePanel>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content(isLoading, error, data)}</div>
+    </div>
+  );
 }
 
 export default AlbumList;
